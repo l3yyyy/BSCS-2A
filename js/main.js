a4b31f778788014903d4701e717638d83fc1e1fa
l3y <dickzonz@gmail.com>
2025-06-05 20:59:58 +0800

    pushhh

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
- function animateProgress(barId, textId, targetPercent) {
-  const fill = document.getElementById(barId).querySelector('.progress-fill');
-  const text = document.getElementById(textId);
-  let current = 0;
+document.getElementById("myLink").addEventListener("click", function(e) {
+  
 
-  const interval = setInterval(() => {
-    if (current > targetPercent) {
-      clearInterval(interval);
-      return;
-    }
-    const angle = (current / 100) * 360;
-    fill.style.background = `conic-gradient(#403D39 ${angle}deg, #CCC5B9 ${angle}deg)`;
-    text.textContent = `${current}%`;
-    current++;
-  }, 20); // speed
-}
+  const about = document.getElementById("about");
 
-function startAllProgress() {
-  animateProgress('progressBarHTML', 'progressTextHTML', 70);
-  animateProgress('progressBarCSS', 'progressTextCSS', 60);
-  animateProgress('progressBarJAVA', 'progressTextJAVA', 75);
-};
+
+  about.classList.remove("slide-in-top");
+  void about.offsetWidth; 
+  about.classList.add("slide-in-top");
+});
+
